b861c46e031a4a58bea847fbdb3d4b22684795ad
Hasan Çalı <72003584+hasancali@users.noreply.github.com>
2022-12-26 14:37:49 +0300

    Create

---
+++ HTMLApi/Controllers/PageController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System.Net.Http.Headers;
+
+namespace HTMLApi.Controllers
+{
+    [ApiController]
+    [Route("api/v1/HTMLApi/[Controller]")]
+    public class PageController : ControllerBase
+    {
+        private readonly ILogger<PageController> _logger;
+
+        public PageController(ILogger<PageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("Upload"), DisableRequestSizeLimit]
+        public async Task<IActionResult> Upload()
+        {
+            try
+            {
+                var file = Request.Form.Files[0];
+
+                var pathToSave = Directory.GetCurrentDirectory();
+                if (file.Length > 0)
+                {
+                    var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+
+                    if (!fileName.Contains(".html"))
+                    {
+                        throw new Exception("Invalid File Format");
+                    }
+
+                    Random x = new Random();
+
+                    fileName = fileName + "_" + x.Next(0, 10000000).ToString();
+
+                    var fullPath = Path.Combine(pathToSave, fileName);
+
+                    var dbPath = Path.Combine(pathToSave, fileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+
+                    return Ok(new { dbPath });
+                }
+                else
+                {
+                    return BadRequest();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        [HttpGet("GetHTML/{FileName}")]
+        public ContentResult GetHTML(string faturaNo)
+        {
+            var html = GetFile(faturaNo);
+
+            return base.Content(html, "text/html");
+        }
+
+        [HttpGet("Download/{FileName}")]
+        public async Task<IActionResult> Download(string FileName)
+        {
+            var fileName = System.IO.Path.GetFileName($@"{Directory.GetCurrentDirectory()}\{FileName}.html");
+            var content = await System.IO.File.ReadAllBytesAsync($@"{Directory.GetCurrentDirectory()}\{FileName}.html");
+            new FileExtensionContentTypeProvider()
+                .TryGetContentType(fileName, out string contentType);
+            return File(content, contentType, fileName);
+        }
+
+        protected string GetFile(string FileName)
+        {
+            var html = System.IO.File.ReadAllText($@"{Directory.GetCurrentDirectory()}\{FileName}.html");
+
+            return html;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # HTMLApi
-HTMLApi
+
+Html file is api written for uploading, downloading and returning
+
+## Page 
+Controller for main model
+
+* **Method:**
+ `POST`
+**​​/api​/v1​/HTMLApi​/Page​/Upload**
+Upload to HTML
+
+* **Method:**
+ `GET`
+**​​/api​/v1​/HTMLApi​/Page​/GetHTML/{FileName}**
+{FileName}.html GET
+
+* **Method:**
+ `GET`
+**​​/api​/v1​/HTMLApi​/Page​/Download/{FileName}**
+Download to {FileName}.html 
